1efe53c841285c1c61db7ba55e37a5f20eb5f5cc
Mouhmad Khalel <mouhmadkhalel@gmail.com>
2021-04-10 21:47:29 +0200

    neue Funktion now you can also change the backgroundcolor

--- QR_Code_Generator1.py
+++ QR_Code_Generator1.py
@@ -1,10 +1,11 @@
 import tkinter
 import qrcode
+import time
 # Basic Code for tk
 app = tkinter.Tk()
 app.title("QR Code Generator")
-app.maxsize(width=500, height=500)
-app.minsize(width=500, height=500)
+app.maxsize(width=300, height=300)
+app.minsize(width=300, height=300)
 #  This def creates, saves and shows a qrCode
 
 
@@ -13,35 +14,44 @@ def create_qr_code(to_save_in_qr, filename):
         version=1, error_correction=qrcode.ERROR_CORRECT_H, box_size=10, border=4,)
     qr.add_data(to_save_in_qr)
     qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
-    img.save(filename)
+    img = qr.make_image(fill_color="black",
+                        back_color=color).convert('RGB')
+    img.save(filename+".png")
+    time.sleep(3)
     img.show()
     print("jo ein qrcode wurde erstellt")
 
-# Get the link and the filename from the input field
+# Get the link , color and filname
 
 
 def link_assignment():
     global to_save_in_qr2
     global filename
+    global color
     to_save_in_qr2 = link_input.get()
     print(to_save_in_qr2)
     filename = filename_input.get()
+    color = back_color_input.get()
 
 
 # Assignment button       OkButton
 assignment_button = tkinter.Button(app, width=20, height=1, text="ok", bg="Orange", command=lambda: [link_assignment(),
                                                                                                      create_qr_code(to_save_in_qr2, filename)])
-assignment_button.place(x=172, y=286)
+assignment_button.place(x=72, y=160)
+# enter the backgroundcolor
+back_color_input = tkinter.Entry(app, bg="Pink", width=24)
+back_color_input.delete(0, "end")
+back_color_input.insert(0, "BackColor")
+back_color_input.place(x=75, y=130)
 # Input Link
 link_input = tkinter.Entry(app, bg="Pink", width=24, textvariable="link")
 link_input.delete(0, "end")
-link_input.insert(0, "Link hier!")
-link_input.place(x=175, y=230)
+link_input.insert(0, "Link ")
+link_input.place(x=75, y=70)
 # Input Filename
 filename_input = tkinter.Entry(app, bg="Pink", width=24)
-filename_input.place(x=175, y=260)
+filename_input.place(x=75, y=100)
 filename_input.delete(0, "end")
-filename_input.insert(0, "filename! Bitte mit Endung")
+filename_input.insert(0, "Filename")
 
 app.mainloop()
